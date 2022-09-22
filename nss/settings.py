6932f13843e25e336aa2ea6302c25067fe2cff4a
Maksymilian <mrowkamaksymilian@gmail.com>
2022-09-22 15:45:38 +0200

    beta 1.0

--- nss/settings.py
+++ nss/settings.py
@@ -126,3 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email backend settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'nssdemo7@gmail.com'
+EMAIL_HOST_PASSWORD = 'quqwcqnvqhfcfrdb'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True 
+DEFAULT_FROM_EMAIL = 'nssdemo7@gmail.com'
